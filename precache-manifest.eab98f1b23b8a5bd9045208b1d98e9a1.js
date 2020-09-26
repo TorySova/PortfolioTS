db94225708009cafefe60f4e9a296b9f7cd9782b
TorySova <toryssova@gmail.com>
2020-09-26 22:06:40 +0300

    Updates

---
+++ precache-manifest.eab98f1b23b8a5bd9045208b1d98e9a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e22813651390529844a739a116d198",
+    "url": "/PortfolioTS/index.html"
+  },
+  {
+    "revision": "c60872f9d0137c487124",
+    "url": "/PortfolioTS/static/css/main.5cd89c61.chunk.css"
+  },
+  {
+    "revision": "c128c8a5654223f21129",
+    "url": "/PortfolioTS/static/js/2.0169d6cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PortfolioTS/static/js/2.0169d6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c60872f9d0137c487124",
+    "url": "/PortfolioTS/static/js/main.f6a5f8b4.chunk.js"
+  },
+  {
+    "revision": "46a35216fdae320f5c84",
+    "url": "/PortfolioTS/static/js/runtime-main.558e075e.js"
+  }
+]);
