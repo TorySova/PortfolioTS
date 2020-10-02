1281fe3b3b3221ddc7bcfbcb256daf57a306f204
TorySova <toryssova@gmail.com>
2020-10-02 15:04:00 +0300

    fix bug

--- src/Greeting/Greeting.tsx
+++ src/Greeting/Greeting.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import style from './Greeting.module.css';
-import styleContainer from '../Common/img/styles/Container.module.css'
+import styleContainer from '../Common/img/styles/Container.module.css';
+import photo from '../Common/img/styles/photo.jpg'
 
 const Greeting = () => {
     return (
         <div className={style.block1}>
-            <div className={styleContainer.container}>
+            <div className={`${styleContainer.container} ${style.greetingContainer}`}>
                 <div className={style.text}>
                     <span>Привет, меня зовут</span>
                     <h1>Виктория Некрасова</h1>
                     <p>и я front-end разработчик!</p>
                 </div>
                 {/* <div className={style.photo}></div> */}
-                <img className={style.photo} src={"https://sun9-10.userapi.com/c623924/v623924423/13d8c/CbipZ1ZdFpM.jpg"} alt=""/>
+                <img className={style.photo} src={photo} alt=""/>
             </div>
         </div>
     );
